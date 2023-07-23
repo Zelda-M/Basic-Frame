9b8fd723c05350e8b94621444dfdc41aca2121fc
Zelda-M <140353767+Zelda-M@users.noreply.github.com>
2023-07-24 00:59:38 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,20 @@
+package basicframeapp;
+
+import za.ac.tut.ul.MyFirstFrame;
+
+public class BasicFrameApp {
+
+    public static void main(String[] args) {
+        
+        //Declare Basic frame
+        MyFirstFrame myframe;
+        
+        //Instantiate the frame
+       myframe = new MyFirstFrame();
+       
+       
+        
+        
+    }
+    
+}
